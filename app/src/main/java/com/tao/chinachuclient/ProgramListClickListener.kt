6b9d0a1e20ad307297340430de220a8e23551c6c
sugtao4423 <sugtao4423@gmail.com>
2022-04-14 03:28:22 +0900

    `ProgramListClickListener` コードフォーマット

--- app/src/main/java/com/tao/chinachuclient/ProgramListClickListener.kt
+++ app/src/main/java/com/tao/chinachuclient/ProgramListClickListener.kt
@@ -9,7 +9,8 @@ import sugtao4423.library.chinachu4j.Program
 import sugtao4423.library.chinachu4j.Recorded
 import sugtao4423.library.chinachu4j.Reserve
 
-class ProgramListClickListener(private val context: Context, private val type: Int) : AdapterView.OnItemClickListener {
+class ProgramListClickListener(private val context: Context, private val type: Int) :
+    AdapterView.OnItemClickListener {
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         if (parent == null) {
@@ -18,9 +19,15 @@ class ProgramListClickListener(private val context: Context, private val type: I
 
         val i = Intent(context, ProgramDetailActivity::class.java)
         when (type) {
-            Type.RESERVES -> i.putExtra("reserve", (parent.getItemAtPosition(position) as Reserve))
-            Type.RECORDED -> i.putExtra("recorded", (parent.getItemAtPosition(position) as Recorded))
-            else -> i.putExtra("program", (parent.getItemAtPosition(position) as Program))
+            Type.RESERVES -> {
+                i.putExtra("reserve", (parent.getItemAtPosition(position) as Reserve))
+            }
+            Type.RECORDED -> {
+                i.putExtra("recorded", (parent.getItemAtPosition(position) as Recorded))
+            }
+            else -> {
+                i.putExtra("program", (parent.getItemAtPosition(position) as Program))
+            }
         }
         i.putExtra("type", type)
         context.startActivity(i)
