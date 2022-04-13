9101a76b213604f0adcbbf213db5ae3dbe379369
sugtao4423 <sugtao4423@gmail.com>
2022-04-14 03:20:52 +0900

    `RuleListAdapter` をBindingに移行

--- app/src/main/java/com/tao/chinachuclient/RuleListAdapter.kt
+++ app/src/main/java/com/tao/chinachuclient/RuleListAdapter.kt
@@ -7,42 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.tao.chinachuclient.databinding.ProgramListLayoutBinding
 import sugtao4423.library.chinachu4j.Rule
 
 class RuleListAdapter(context: Context) :
     ArrayAdapter<Rule>(context, android.R.layout.simple_list_item_1) {
 
-    private val mInflater =
-        context.getSystemService(AppCompatActivity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     private val oldCategoryColor =
         (context.applicationContext as App).currentServer.oldCategoryColor
 
-    private data class ViewHolder(
-        val title: TextView,
-        val channel: TextView
-    )
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var view = convertView
-        val holder: ViewHolder
-        if (view == null) {
-            view = mInflater.inflate(R.layout.program_list_layout, parent, false)
-            val title = view.findViewById<TextView>(R.id.programTitle)
-            val channel = view.findViewById<TextView>(R.id.programDate)
-
-            holder = ViewHolder(title, channel)
-            view.tag = holder
+        val binding = if (convertView == null) {
+            val inflater = LayoutInflater.from(context)
+            val tBinding = ProgramListLayoutBinding.inflate(inflater, parent, false)
+            tBinding.root.tag = tBinding
+            tBinding
         } else {
-            holder = view.tag as ViewHolder
+            convertView.tag as ProgramListLayoutBinding
         }
 
-        val item = getItem(position) ?: return view!!
+        val item = getItem(position) ?: return binding.root
 
         if (item.categories.isNotEmpty()) {
-            view?.setBackgroundResource(
+            binding.root.setBackgroundResource(
                 if (oldCategoryColor) {
                     when (item.categories[0]) {
                         "anime" -> R.drawable.old_anime
@@ -75,31 +63,30 @@ class RuleListAdapter(context: Context) :
 
         val title = item.reserveTitles.let { if (it.isEmpty()) "any" else it.joinToString() }
         val channel = item.channels.let { if (it.isEmpty()) "any" else it.joinToString() }
-
-        holder.title.text = title
-        holder.channel.text = channel
+        binding.programTitle.text = title
+        binding.programDate.text = channel
 
         if (item.isDisabled) {
-            holder.title.let {
+            binding.programTitle.let {
                 it.setTextColor(Color.GRAY)
                 it.paint.flags = it.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             }
-            holder.channel.let {
+            binding.programDate.let {
                 it.setTextColor(Color.GRAY)
                 it.paint.flags = it.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             }
         } else {
-            holder.title.let {
+            binding.programTitle.let {
                 it.setTextColor(ContextCompat.getColor(context, R.color.titleText))
                 it.paint.flags = it.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
             }
-            holder.channel.let {
+            binding.programDate.let {
                 it.setTextColor(ContextCompat.getColor(context, R.color.dateText))
                 it.paint.flags = it.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
             }
         }
 
-        return view!!
+        return binding.root
     }
 
     override fun addAll(rules: Array<Rule>) {
